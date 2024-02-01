909710a2908076dd177ed34b0523a2449d0b793d
Divyansh Chauhan <106025754+Quarkblue@users.noreply.github.com>
2024-02-01 22:05:14 +0530

    Update MoCap.py

--- G.ONE/Assets/StreamingAssets/PythonScripts/MoCap.py
+++ G.ONE/Assets/StreamingAssets/PythonScripts/MoCap.py
@@ -21,9 +21,7 @@ pose_video = mp_pose.Pose(static_image_mode=False, model_complexity=1, min_detec
 # Initialize mediapipe drawing class.
 mp_drawing = mp.solutions.drawing_utils
 
-#init udp socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serverAddressPort = ("127.0.0.1", 9999)
+
 
 
 def detectPose(image, pose, draw=False, display=False):
@@ -132,48 +130,174 @@ def SingleTakeClassification():
     
 
 def videoClassification():
-    camera_video = cv2.VideoCapture(0)
-    camera_video.set(3,1280)
-    camera_video.set(4,960)
-    flag = False
-    while camera_video.isOpened():
     
-        # Read a frame.
-        ok, frame = camera_video.read()
+    #init udp socket vars
+    positionAddressPort = ("127.0.0.1", 9999)
+    jumpAddressPort = ("127.0.0.1", 6969)
+    
+    #init udp socket
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverAddressPort = ("127.0.0.1", 9999)
+    
+    
+    
+    cap = cv2.VideoCapture(0)
+    cap.set(3,1280)
+    cap.set(4,960)
+    flag = False
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
         
-        # Check if frame is not read properly then continue to the next iteration to read the next frame.
-        if not ok:
-            continue
-        
-        # Flip the frame horizontally for natural (selfie-view) visualization.
-        frame = cv2.flip(frame, 1)
-        
-        # Get the height and width of the frame of the webcam video.
-        frame_height, frame_width, _ = frame.shape
-        
-        # Perform the pose detection on the frame.
-        frame, results = detectPose(frame, pose_video, draw=True)
-        
-        # Check if the pose landmarks in the frame are detected.
-        if results.pose_landmarks:
-                
-            # Check the horizontal position of the person in the frame.
-            position = checkLeftRight(frame, results)
+            # Read a frame.
+            ok, frame = cap.read()
             
-            # Print the position to the console.
-            if(not flag):
-                print("#############Started Capturing#############")
-                flag = True
-            sock.sendto(str.encode(str("Hello from python")), serverAddressPort)
-            sock.sendto(str.encode(str(position)), serverAddressPort)
-
+            if not ok:
+                continue
+            
+            frame = cv2.flip(frame, 1)
+            
+            frame_height, frame_width, _ = frame.shape
+            
+            frame, results = detectPose(frame, pose_video, draw=True)
+            
+            if results.pose_landmarks:
                     
-        # Wait for 1ms. If a a key is pressed, retreive the ASCII code of the key.
-        k = cv2.waitKey(1) & 0xFF
+                position = checkLeftRight(frame, results)
+                
+                if(not flag):
+                    print("#############Started Capturing#############")
+                    flag = True
+                # sock.sendto(str.encode(str("Hello from python")), serverAddressPort)
+                sock.sendto(str.encode(str(position)), positionAddressPort)
+                
+                
+
+            try:
+                landmarks = results.pose_landmarks.landmark
+                # shoulderleft = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                elbowleft = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                wristleft = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                # shoulderright = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                elbowright = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                wristright = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+                    
+                if wristleft[1] < elbowleft[1] and wristright[1] < elbowright[1]:
+                    hand_position = "both hands up"
+                elif wristleft[1] < elbowleft[1]:
+                    hand_position = "left hand up"
+                elif wristright[1] < elbowright[1]:
+                    hand_position = "right hand up"
+                else:
+                    hand_position = "both hands down"
+
+                # Print positions to console
+                print(hand_position)
+                sock.sendto(str.encode(str(hand_position)), jumpAddressPort)
+            except:
+                pass
+
+                        
+            k = cv2.waitKey(1) & 0xFF
+            
+            if(k == 27):
+                break
+
+
+def detect_pose():
+    
+    # create socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverAddressPort = ("127.0.0.1", 6969)
+    
+    mp_pose = mp.solutions.pose
+    mp_drawing = mp.solutions.drawing_utils
+
+    cap = cv2.VideoCapture(0)
+
+    # Curl counter variables
+    counter = 0 
+    stage = None
+
+    ## Setup mediapipe instance
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+          
+            # Make detection
+            results = pose.process(image)
         
-        # Check if 'ESC' is pressed and break the loop.
-        if(k == 27):
-            break
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            
+            # Extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+                # shoulderleft = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                elbowleft = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                wristleft = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                # shoulderright = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                elbowright = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                wristright = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+                    
+                if wristleft[1] < elbowleft[1] and wristright[1] < elbowright[1]:
+                    hand_position = "both hands up"
+                elif wristleft[1] < elbowleft[1]:
+                    hand_position = "left hand up"
+                elif wristright[1] < elbowright[1]:
+                    hand_position = "right hand up"
+                else:
+                    hand_position = "both hands down"
+
+                # Print positions to console
+                print(hand_position)
+                sock.sendto(str.encode(str(hand_position)), serverAddressPort)
+                
+
+
+                
+
+            except:
+                pass
+            
+            # Setup status box
+             # Setup status box
+            cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+            
+            # Rep data
+            cv2.putText(image, 'REPS', (15,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(counter), 
+                        (10,60), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            
+            # Stage data
+            cv2.putText(image, 'STAGE', (65,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, stage, 
+                        (60,60), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            
+            
+            # Render detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                    )               
+            
+            cv2.imshow('Mediapipe Feed', image)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break   
+            
+        cap.release()
+        cv2.destroyAllWindows()
+        print("Done")
 
 
 if(__name__ == '__main__'):
