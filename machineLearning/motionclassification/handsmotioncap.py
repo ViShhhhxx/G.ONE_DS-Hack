e96ae81d4f80334bd50ffdaee2fd4a1eb7b536c3
mkswagger <mathangykrishna16@gmail.com>
2024-02-01 20:08:03 +0530

    armpositionclassification done ✨

---
+++ machineLearning/motionclassification/handsmotioncap.py
@@ -0,0 +1,93 @@
+import cv2
+import mediapipe as mp
+
+def detect_pose():
+    mp_pose = mp.solutions.pose
+    mp_drawing = mp.solutions.drawing_utils
+
+    cap = cv2.VideoCapture(0)
+
+    # Curl counter variables
+    counter = 0 
+    stage = None
+
+    ## Setup mediapipe instance
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+          
+            # Make detection
+            results = pose.process(image)
+        
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            
+            # Extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+                shoulderleft = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                elbowleft = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                wristleft = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                shoulderright = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                elbowright = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                wristright = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+                    
+                if wristleft[1] < elbowleft[1] and wristright[1] < elbowright[1]:
+                    hand_position = "both hands up"
+                elif wristleft[1] < elbowleft[1]:
+                    hand_position = "left hand up"
+                elif wristright[1] < elbowright[1]:
+                    hand_position = "right hand up"
+                else:
+                    hand_position = "both hands down"
+
+                # Print positions to console
+                print(hand_position)
+
+
+                
+
+            except:
+                pass
+            
+            # Setup status box
+             # Setup status box
+            cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+            
+            # Rep data
+            cv2.putText(image, 'REPS', (15,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(counter), 
+                        (10,60), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            
+            # Stage data
+            cv2.putText(image, 'STAGE', (65,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, stage, 
+                        (60,60), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            
+            
+            # Render detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                    )               
+            
+            cv2.imshow('Mediapipe Feed', image)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break   
+            
+        cap.release()
+        cv2.destroyAllWindows()
+        print("Done")
+           
+      
+detect_pose()
