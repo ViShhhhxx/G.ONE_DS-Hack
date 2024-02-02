d1eee90ab61e865d5f1e708a8b7c3f9e7ed53fba
Divyansh Chauhan <106025754+Quarkblue@users.noreply.github.com>
2024-02-02 07:59:11 +0530

    build

---
+++ Build/MotionCapture_Data/StreamingAssets/PythonScripts/MoCap.py
@@ -0,0 +1,305 @@
+from unittest import result
+import cv2
+import time
+# from math import hypot
+import mediapipe as mp
+import matplotlib.pyplot as plt
+import socket
+# import handsmotioncap as hmc
+
+#initialise mediapipe
+
+mp_pose = mp.solutions.pose
+
+# Setup the Pose function for images.
+pose_image = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5, model_complexity=1)
+
+# Setup the Pose function for videos.
+pose_video = mp_pose.Pose(static_image_mode=False, model_complexity=1, min_detection_confidence=0.7,
+                          min_tracking_confidence=0.7)
+
+# Initialize mediapipe drawing class.
+mp_drawing = mp.solutions.drawing_utils
+
+
+
+
+def detectPose(image, pose, draw=False, display=False):
+    
+    
+    # Create a copy of the input image.
+    output_image = image.copy()
+    
+    # Convert the image from BGR into RGB format.
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    # Perform the Pose Detection.
+    results = pose.process(imageRGB)
+    
+    # Check if any landmarks are detected and are specified to be drawn.
+    if results.pose_landmarks and draw:
+    
+        # Draw Pose Landmarks on the output image.
+        mp_drawing.draw_landmarks(image=output_image, landmark_list=results.pose_landmarks,
+                                  connections=mp_pose.POSE_CONNECTIONS,
+                                  landmark_drawing_spec=mp_drawing.DrawingSpec(color=(255,255,255),
+                                                                               thickness=3, circle_radius=3),
+                                  connection_drawing_spec=mp_drawing.DrawingSpec(color=(49,125,237),
+                                                                               thickness=2, circle_radius=2))
+
+    # Check if the original input image and the resultant image are specified to be displayed.
+    if display:
+    
+        # Display the original input image and the resultant image.
+        plt.figure(figsize=[22,22])
+        plt.subplot(121);plt.imshow(image[:,:,::-1]);plt.title("Original Image");plt.axis('off');
+        plt.subplot(122);plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
+        
+    # Otherwise
+    else:
+
+        # Return the output image and the results of pose landmarks detection.
+        return output_image, results
+    
+
+
+def checkLeftRight(image, results):
+   
+    # Declare a variable to store the horizontal position (left, center, right) of the person.
+    horizontal_position = None
+    
+    # Get the width of the image.
+    _, width, _ = image.shape
+    
+    # Retreive the x-coordinate of the left shoulder landmark.
+    left_x = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].x * width)
+
+    # Retreive the x-corrdinate of the right shoulder landmark.
+    right_x = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].x * width)
+    
+    # Check if the person is at left that is when both shoulder landmarks x-corrdinates
+    # are less than or equal to the x-corrdinate of the center of the image.
+    if (right_x <= width//2 and left_x <= width//2):
+        
+        # Set the person's position to left.
+        horizontal_position = 'Left'
+
+    # Check if the person is at right that is when both shoulder landmarks x-corrdinates
+    # are greater than or equal to the x-corrdinate of the center of the image.
+    elif (right_x >= width//2 and left_x >= width//2):
+        
+        # Set the person's position to right.
+        horizontal_position = 'Right'
+    
+    # Check if the person is at center that is when right shoulder landmark x-corrdinate is greater than or equal to
+    # and left shoulder landmark x-corrdinate is less than or equal to the x-corrdinate of the center of the image.
+    elif (right_x >= width//2 and left_x <= width//2):
+        
+        # Set the person's position to center.
+        horizontal_position = 'Center'
+    
+    # Return the person's horizontal position.
+    return horizontal_position
+
+import os
+import glob
+
+camera_video = cv2.VideoCapture(0)
+camera_video.set(3,1280)
+camera_video.set(4,960)
+
+def SingleTakeClassification():
+
+    ok, frame = camera_video.read()
+    
+    if not ok:
+        return
+    
+    frame = cv2.flip(frame, 1)
+    
+    frame_height, frame_width, _ = frame.shape
+    
+    frame, results = detectPose(frame, pose_video, draw=True)
+    
+    if results.pose_landmarks:
+        
+        position = checkLeftRight(frame, results)
+        # position = "right"
+        
+        return position
+    
+
+def videoClassification():
+    
+    #init udp socket vars
+    positionAddressPort = ("127.0.0.1", 9999)
+    jumpAddressPort = ("127.0.0.1", 6969)
+    
+    #init udp socket
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverAddressPort = ("127.0.0.1", 9999)
+    
+    
+    
+    cap = cv2.VideoCapture(0)
+    cap.set(3,1280)
+    cap.set(4,960)
+    flag = False
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+        
+            # Read a frame.
+            ok, frame = cap.read()
+            
+            if not ok:
+                continue
+            
+            frame = cv2.flip(frame, 1)
+            
+            frame_height, frame_width, _ = frame.shape
+            
+            frame, results = detectPose(frame, pose_video, draw=True)
+            
+            if results.pose_landmarks:
+                    
+                position = checkLeftRight(frame, results)
+                
+                if(not flag):
+                    print("#############Started Capturing#############")
+                    flag = True
+                # sock.sendto(str.encode(str("Hello from python")), serverAddressPort)
+                sock.sendto(str.encode(str(position)), positionAddressPort)
+                
+                
+
+            try:
+                landmarks = results.pose_landmarks.landmark
+                # shoulderleft = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                elbowleft = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                wristleft = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                # shoulderright = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                elbowright = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                wristright = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+                    
+                if wristleft[1] < elbowleft[1] and wristright[1] < elbowright[1]:
+                    hand_position = "Jumping"
+                elif wristleft[1] < elbowleft[1]:
+                    hand_position = "left hand up"
+                elif wristright[1] < elbowright[1]:
+                    hand_position = "right hand up"
+                else:
+                    hand_position = "both hands down"
+
+                # Print positions to console
+                print(hand_position)
+                sock.sendto(str.encode(str(hand_position)), jumpAddressPort)
+            except:
+                pass
+
+                        
+            k = cv2.waitKey(1) & 0xFF
+            
+            if(k == 27):
+                break
+
+
+def detect_pose():
+    
+    # create socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverAddressPort = ("127.0.0.1", 6969)
+    
+    mp_pose = mp.solutions.pose
+    mp_drawing = mp.solutions.drawing_utils
+
+    cap = cv2.VideoCapture(0)
+
+    # Curl counter variables
+    counter = 0 
+    stage = None
+
+    ## Setup mediapipe instance
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+          
+            # Make detection
+            results = pose.process(image)
+        
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            
+            # Extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+                # shoulderleft = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                elbowleft = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                wristleft = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                # shoulderright = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                elbowright = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                wristright = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+                    
+                if wristleft[1] < elbowleft[1] and wristright[1] < elbowright[1]:
+                    hand_position = "both hands up"
+                elif wristleft[1] < elbowleft[1]:
+                    hand_position = "left hand up"
+                elif wristright[1] < elbowright[1]:
+                    hand_position = "right hand up"
+                else:
+                    hand_position = "both hands down"
+
+                # Print positions to console
+                print(hand_position)
+                sock.sendto(str.encode(str(hand_position)), serverAddressPort)
+                
+
+
+                
+
+            except:
+                pass
+            
+            # Setup status box
+             # Setup status box
+            cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+            
+            # Rep data
+            cv2.putText(image, 'REPS', (15,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(counter), 
+                        (10,60), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            
+            # Stage data
+            cv2.putText(image, 'STAGE', (65,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, stage, 
+                        (60,60), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            
+            
+            # Render detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                    )               
+            
+            cv2.imshow('Mediapipe Feed', image)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break   
+            
+        cap.release()
+        cv2.destroyAllWindows()
+        print("Done")
+
+
+if(__name__ == '__main__'):
+    videoClassification()
+    # hmc.detect_pose()
